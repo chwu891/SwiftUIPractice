8efdce836e853ffe051139c49d0103b3d2e901dd
Chi-Hsien Wu <chwu891@gmail.com>
2025-07-10 18:40:28 -0700

    Initial Commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Chi-Hsien Wu on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIPracticeTests/SwiftUIPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeTests.swift
+//  SwiftUIPracticeTests
+//
+//  Created by Chi-Hsien Wu on 7/10/25.
+//
+
+import Testing
+@testable import SwiftUIPractice
+
+struct SwiftUIPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
